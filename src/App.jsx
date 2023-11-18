4c6e8d6394726d2d5ced69f5acd51cfb10ebc2e0
M.Kristóf <drzero123456@gmail.com>
2023-11-18 19:12:26 +0100

    11.18

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,48 @@ import styled from "styled-components";
 
 // API link: https://hp-api.onrender.com/
 
+const COLORS = {
+  Gryffindor: "#a02727",
+  Slytherin: "#257b3f",
+  Ravenclaw: "#0598b6",
+  Hufflepuff: "#cb9c27",
+};
+
+const StyledCard = styled.div`
+  background-color: ${(props) =>
+    props.house === "Slytherin"
+      ? COLORS.Slytherin
+      : props.house === "Gryffindor"
+      ? COLORS.Gryffindor
+      : props.house === "Ravenclaw"
+      ? COLORS.Ravenclaw
+      : props.house === "Hufflepuff"
+      ? COLORS.Hufflepuff
+      : null};
+  border: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  padding: 2em;
+`;
+
 export const Card = ({ wizard_data }) => {
-  const { name } = wizard_data;
-  return <h1>{name}</h1>;
+  const { name, house } = wizard_data;
+  return (
+    <StyledCard house={house}>
+      {wizard_data.image && (
+        <img
+          src={wizard_data.image}
+          style={{
+            height: "400px",
+            width: "400px",
+            objectFit: "cover",
+            order: -1,
+          }}
+        />
+      )}
+      <h1>{name}</h1>
+    </StyledCard>
+  );
 };
 
 function App() {
@@ -16,7 +55,7 @@ function App() {
   const [maximumScore, setMaximumScore] = useState(0);
   const [activeCards, setActiveCards] = useState([]);
   const [selectedCards, setSelectedCards] = useState([]);
-  const [level, setLevel] = useState(5);
+  const [level, setLevel] = useState(1);
   const [isGameOver, setIsGameOver] = useState(false);
   const [isWinner, setIsWinner] = useState(false);
 
@@ -45,10 +84,10 @@ function App() {
     setActiveCards(copy_arr);
   };
 
-  const fetchData = async () => {
-    await fetch("https://hp-api.onrender.com/api/characters")
-      .then(async (response) => response.json())
-      .then(async (data) => {
+  const fetchData = () => {
+    fetch("https://hp-api.onrender.com/api/characters")
+      .then((response) => response.json())
+      .then((data) => {
         const arr_range = 24;
         let copy_arr = [];
         let used_indexes = [];
